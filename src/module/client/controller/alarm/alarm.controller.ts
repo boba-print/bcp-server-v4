a61291043b9f905a6c74960b47f99871b17305d5
LEE KYU WON <duffufk1202@naver.com>
2022-06-25 16:02:08 +0900

    Record user

--- src/module/client/controller/alarm/alarm.controller.ts
+++ src/module/client/controller/alarm/alarm.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Query, Req } from '@nestjs/common';
 import { PrintOrderService } from 'src/module/client/service/print-order/print-order.service';
+import { PrismaService } from 'src/service/prisma.service';
 import { ClientAuthRequest } from '../../ClientAuthRequest';
 
 @Controller('alarm')
 export class AlarmController {
-  constructor(private readonly printOrderService: PrintOrderService) {}
+  constructor(
+    private readonly printOrderService: PrintOrderService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
   async getAll(
@@ -13,7 +17,7 @@ export class AlarmController {
     @Query('n')
     numLimit: number,
   ) {
-    // 현재는 프린트 내역만 알람으로 가져온다. 추후에 공지사항과 기타 알림 기능도 알람에 포함되어야 함.
+    // TODO: 현재는 프린트 내역만 알람으로 가져온다. 추후에 공지사항과 기타 알림 기능도 알람에 포함되어야 함.
     const { user } = req;
     const printOrders = await this.printOrderService.findMany(
       {
@@ -22,6 +26,14 @@ export class AlarmController {
       0,
       numLimit,
     );
+
+    // 유저가 마지막으로 공지를 읽은 시간을 기록한다.
+    await this.prismaService.users.update({
+      where: { UserID: req.user.UserID },
+      data: {
+        CheckedNoticeAt: new Date(),
+      },
+    });
     return printOrders.map((po) => po.props);
   }
 }
