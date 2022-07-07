ccf6b12a3b175c470e1546fc33167f8499a6cfaa
Dong <rlaehddnr1009@naver.com>
2022-07-07 11:27:07 +0900

    mod user/card apit

--- src/module/card/card.controller.ts
+++ src/module/card/card.controller.ts
@@ -8,20 +8,11 @@ export class CardController {
 
   @Get(':userId/cards')
   @UseGuards(UserAuthGuard)
-  async findMany(@Param('userId') userId: string, @Query('n') n: string) {
-    let numLimit = parseInt(n);
-    if (isNaN(numLimit)) {
-      console.warn(
-        '[CardController.findMany] parsing number error, set to default 10',
-      );
-      numLimit = 10;
-    }
-
+  async findMany(@Param('userId') userId: string) {
     const cards = await this.prismaService.cards.findMany({
       where: {
         UserID: userId,
       },
-      take: numLimit,
       orderBy: {
         CreatedAt: 'desc',
       },
