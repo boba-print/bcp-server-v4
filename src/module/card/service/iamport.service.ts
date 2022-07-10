0a263c5f253ce60eb4c5b28981840d4950b5d80e
LEE KYU WON <duffufk1202@naver.com>
2022-07-10 21:50:04 +0900

    Add card subscribe

--- src/module/card/service/iamport.service.ts
+++ src/module/card/service/iamport.service.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { InvalidEnv } from 'src/common/error';
 import { IamportGetTokenError, IamportSubscribeError } from '../error';
 import { IamportGetTokenResponse } from '../dto/IamportGetTokenResponse.dto';
+import { IamportSubscribeResponseDto } from '../dto/IamportSubscribeResponse.dto';
 
 @Injectable()
 export class IamportService {
@@ -18,7 +19,7 @@ export class IamportService {
     const { IMP_CODE, IMP_REST_API, IMP_API_SECRET } = process.env;
 
     if (!IMP_CODE || !IMP_REST_API || !IMP_API_SECRET) {
-      throw new InvalidEnv('Invalid env var');
+      throw new InvalidEnv('Iamport env missed');
     }
 
     this.IMP_REST_API = IMP_REST_API;
@@ -26,28 +27,40 @@ export class IamportService {
   }
 
   async addSubscriber(customerId: string, dto: CardCreateDto) {
-    const request = await this.getAuthenticatedInstance();
+    const options = await this.getAuthenticatedAxiosOptions();
+    const response = await axios
+      .post<IamportSubscribeResponseDto>(
+        `subscribe/customer/${customerId}`,
+        {
+          card_number: dto.cardNumber,
+          expiry: dto.expiry,
+          birth: dto.birth,
+          pwd_2digit: dto.pwd2digit,
+        },
+        options,
+      )
+      .catch((error) => {
+        throw new IamportSubscribeError(error.message);
+      });
 
-    const response = await request({
-      url: `subscribe/customer/${customerId}`,
-      method: 'POST',
-      data: {
-        card_number: dto.cardNumber,
-        expiry: dto.expiry,
-        birth: dto.birth,
-        pwd_2digit: dto.pwd2digit,
-      },
-    }).catch((error) => {
-      throw new IamportSubscribeError(error.message);
-    });
+    const { data } = response;
+    if (data.code !== 0) {
+      throw new IamportSubscribeError(data.message);
+    }
+
+    return data;
   }
 
   private async fetchToken() {
     const response = await axios
-      .post<IamportGetTokenResponse>(`${this.BASE_URL}/users/getToken`, {
-        imp_key: this.IMP_REST_API,
-        imp_secret: this.IMP_API_SECRET,
-      })
+      .post<IamportGetTokenResponse>(
+        'users/getToken',
+        {
+          imp_key: this.IMP_REST_API,
+          imp_secret: this.IMP_API_SECRET,
+        },
+        { baseURL: this.BASE_URL },
+      )
       .catch((err) => {
         throw new IamportGetTokenError(err.message);
       });
@@ -60,18 +73,18 @@ export class IamportService {
     this.expireAt = new Date((dto.response.expired_at - 60) * 1000); // 토큰 만료 1분전에 미리 refresh 하기 위해
   }
 
-  private async getAuthenticatedInstance() {
+  private async getAuthenticatedAxiosOptions() {
     if (this.isTokenAvailable) {
       await this.fetchToken();
     }
 
-    return axios.create({
-      url: this.BASE_URL,
+    return {
+      baseURL: this.BASE_URL,
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${this.accessToken}`,
       },
-    });
+    };
   }
 
   private get isTokenAvailable() {
